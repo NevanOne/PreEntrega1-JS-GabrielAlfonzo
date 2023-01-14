57572e61a2712a49d9f3df720fc5575ddb5e399a
NevanOne <gaalfonzo@gmail.com>
2023-01-14 11:58:48 -0300

    Condicional en caso de monto negativo.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
+// Crear la funcion para convertir el tipo de moneda
+ function convert() {
+  const amount = document.getElementById("amount").value;
+  const fromCurrency = document.getElementById("fromCurrency").value;
+  const toCurrency = document.getElementById("toCurrency").value;
+  
+  //Valores de los tipos de cambio
+  const exchangeRates = {
+    USD: 1,
+    EUR: 1.2,
+    GBP: 1.3
+  };
+  
+  const exchangeRate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
+  const convertedAmount = amount * exchangeRate;
+  
+  // En caso de que el monto escrito sea negativo, mostrará una alerta para que el usuario corrija su error
 
-  // Crear una función para la conversión de monedas
-  function convert() {
-    // Obtener el monto, la moneda de origen y la moneda de destino del formulario
-    const amount = document.getElementById("amount").value;
-    const fromCurrency = document.getElementById("fromCurrency").value;
-    const toCurrency = document.getElementById("toCurrency").value;
-    
-    // Crea un objeto con las tasas de cambio fijas entre las monedas
-    const exchangeRates = {
-      USD: 1,
-      EUR: 1.2,
-      GBP: 1.3
-    };
-    
-    // Obtener la tasa de cambio entre la moneda de origen y la moneda de destino
-    const exchangeRate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
-    
-    // Aplicar la tasa de cambio al monto dado
-    const convertedAmount = amount * exchangeRate;
-    
-    // Mostrar el monto convertido en el elemento "result"
-    document.getElementById("result").innerHTML = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+  if (amount > 0) {
+      document.getElementById("result").innerHTML = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+  } else {
+      document.getElementById("result").innerHTML = "Ingrese un monto positivo";
   }
+}
